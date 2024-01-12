23f0f4b6a949935be61e846d22bffe38b5cd1adf
JLuong215 <jimmy4865545@gmail.com>
2024-01-11 23:04:08 -0500

    Candidate testing

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -5,17 +5,34 @@ let shuttleCabinReady = true
 let crewStatus = spaceSuitsOn && shuttleCabinReady;
 let computerStatusCode = 200;
 let shuttleSpeed = 15000;
+//Shuttle Launch
+let date = ("Monday, 2019-03-18");
+let time = ("10:05:34 am");
+let astronautCount = 7;
+let astronautStatus = ("ready");
+let averageAstronautMassKg = 80.7;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let crewMassKg = astronautCount*averageAstronautMassKg;
+let totalMassKg = crewMassKg+fuelMassKg+shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = 100;
+let weatherStatus = ("clear");
+let preparedForLiftOff = true;
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 
-if (totalNumberOfCrew <= 7) {
+if (astronautCount <= 7) {
     console.log("Launch");
 } else {
     console.log ("Do Not Launch");
 }
 // add logic below to verify all astronauts are ready
 
-if (crewStatus == "ready") {
+if (astronautStatus == "ready") {
     console.log("All Astronauts are ready");
 } else {
     console.log("Not Ready");
@@ -23,14 +40,14 @@ if (crewStatus == "ready") {
 
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
 
-if (totalMass >= 85000) {
+if (totalMassKg >= 85000) {
     console.log("Maximum limit not exceeded");
 } else {
     console.log("Weight exceeded");
 }
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
 
-if (fuelTemperature <= -150 && fuelTemperature >= -300){
+if (fuelTempCelsius <= -150 && fuelTempCelsius >= -300){
     console.log("Fuel Temp is good");
 } else if (fuelTemperature >= -150) {
     console.log("it is too hot");
@@ -46,15 +63,25 @@ if (fuelLevel == 100) {
 }
 // add logic below to verify the weather status is clear
 
-if (Weather == Clear) {
+if (weatherStatus == Clear) {
     console.log("Weather is good for launch");
 } else {
     console.log("Weather is not suitable for launch");
 }
 // Verify shuttle launch can proceed based on above conditions
 
-if (totalNumberOfCrew <= 7 && crewStatus == "ready" && totalMass >= 85000 && fuelTemperature <= -150 && fuelTemperature >= -300 && fuelLevel == 100 && Weather == Clear) {
-    console.log("Shuttle can launch");
+if (astronautCount <= 7 && astronautStatus == "ready" && maximumMassLimit >= 85000 && maximumFuelTemp <= -150 && minimumFuelTemp >= -300 && fuelLevel == 100 && weatherStatus == Clear) {
+    console.log("Have a safe flight.");
 } else {
     console.log("Shuttle cannot launch");
 }
+console.log("All Systems are a go? initialting space Shuttle launch Sequence.");
+console.log("----------------------------------------------------------------");
+Console.log("Date: " + date);
+console.log("Time: " + time);
+console.log("Astronaut count: " + astronautCount);
+console.log("Crew Mass: " + crewMassKg);
+console.log("Fuel Mass: " + fuelMassKg);
+console.log("Shuttle Mass: " + totalMassKg);
+console.log("fuelTempCelsius: " + fuelTempCelsius);
+console.log("weatherStatus: " + weatherStatus);
